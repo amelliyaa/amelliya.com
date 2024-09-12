7c44032ab5ec70d4657840aa9f42c77ffa0e2f15
amelliyaa <143849648+amelliyaa@users.noreply.github.com>
2024-09-12 09:12:16 -0400

    add homepage structure and base styling

---
+++ src/schemas.ts
@@ -0,0 +1,30 @@
+import { z } from "astro:content";
+
+export const postSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  updated: z.date(),
+  published: z.date(),
+  thumbnail: z.object({
+    imagePath: z.string(),
+    alt: z.string(),
+  }),
+  tags: z.array(z.string()),
+});
+
+export const gameSchema = z.object({
+  route: z.string(),
+  title: z.string(),
+  keyArt: z.object({
+    imagePath: z.string(),
+    alt: z.string(),
+  }),
+});
+
+export const projectSchema = z.object({
+  workingTitle: z.string(),
+  description: z.string(),
+  progress: z.string(),
+  updated: z.date(),
+  pinSize: z.string(),
+});
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,63 +1,63 @@
-const mobileMenuButton = document.getElementById("mobile-menu-button");
-const navMenu = document.getElementById("nav-menu");
-const menuItems = navMenu.querySelectorAll("li > a");
-const breakpoint = window.matchMedia("(min-width: 36rem)");
+document.addEventListener("astro:page-load", () => {
+  const mobileMenuButton = document.getElementById("mobile-menu-button");
+  const navMenu = document.getElementById("nav-menu");
+  const menuItems = navMenu.querySelectorAll("li > a");
+  const breakpoint = window.matchMedia("(min-width: 36rem)");
 
-const toggleDropdown = () => {
-  navMenu.classList.toggle("show");
+  const toggleDropdown = () => {
+    navMenu.classList.toggle("show");
 
-  if (mobileMenuButton.getAttribute("aria-expanded") === "false") {
-    // dropdown was opened
-    mobileMenuButton.setAttribute("aria-expanded", "true");
-    setScrolling(true);
-    addToFocusOrder();
-  } else {
-    // dropdown was closed
-    mobileMenuButton.setAttribute("aria-expanded", "false");
-    setScrolling(false);
-    removeFromFocusOrder();
-  }
-};
-
-const addToFocusOrder = () => {
-  menuItems.forEach((menuItem) => {
-    menuItem.removeAttribute("tabIndex");
-  });
-};
-
-const removeFromFocusOrder = () => {
-  menuItems.forEach((menuItem) => {
-    menuItem.tabIndex = -1;
-  });
-};
-
-const setScrolling = (bool) => {
-  document.body.classList.toggle("no-scroll", bool);
-};
-
-mobileMenuButton.addEventListener("click", toggleDropdown);
-
-breakpoint.addEventListener("change", ({ matches: isWide }) => {
-  if (mobileMenuButton.getAttribute("aria-expanded") === "true") {
-    // dropdown was visible
-    if (isWide) {
-      setScrolling(false);
-      addToFocusOrder();
-    } else {
+    if (mobileMenuButton.getAttribute("aria-expanded") === "false") {
+      // dropdown was opened
+      mobileMenuButton.setAttribute("aria-expanded", "true");
       setScrolling(true);
-    }
-  } else {
-    // dropdown was not visible
-    if (isWide) {
       addToFocusOrder();
     } else {
+      // dropdown was closed
+      mobileMenuButton.setAttribute("aria-expanded", "false");
+      setScrolling(false);
       removeFromFocusOrder();
     }
-  }
-});
+  };
 
-window.addEventListener("load", () => {
+  const addToFocusOrder = () => {
+    menuItems.forEach((menuItem) => {
+      menuItem.removeAttribute("tabIndex");
+    });
+  };
+
+  const removeFromFocusOrder = () => {
+    menuItems.forEach((menuItem) => {
+      menuItem.tabIndex = -1;
+    });
+  };
+
+  const setScrolling = (bool) => {
+    document.body.classList.toggle("no-scroll", bool);
+  };
+
   if (!breakpoint.matches) {
     removeFromFocusOrder();
   }
+
+  mobileMenuButton.addEventListener("click", toggleDropdown);
+
+  breakpoint.addEventListener("change", ({ matches: isWide }) => {
+    if (mobileMenuButton.getAttribute("aria-expanded") === "true") {
+      // dropdown was visible
+      if (isWide) {
+        setScrolling(false);
+        addToFocusOrder();
+      } else {
+        setScrolling(true);
+      }
+    } else {
+      // dropdown was not visible
+      if (isWide) {
+        addToFocusOrder();
+      } else {
+        removeFromFocusOrder();
+      }
+    }
+  });
 });
