4aa6fafce47f448d31eab5edbf7720174eaef3aa
amelliyaa <143849648+amelliyaa@users.noreply.github.com>
2026-05-30 21:47:26 -0400

    add astro font component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, svgoOptimizer } from "astro/config";
+import { defineConfig, fontProviders, svgoOptimizer } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -21,7 +21,68 @@ export default defineConfig({
   image: {
     layout: "constrained",
   },
-
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Philosopher",
+      cssVariable: "--typeface-logo",
+      options: {
+        variants: [
+          {
+            weight: "700",
+            style: "normal",
+            src: ["./src/assets/fonts/Philosopher-Bold.woff2"],
+            fallback: ["sans-serif"],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Merriweather Sans",
+      cssVariable: "--typeface-display",
+      options: {
+        variants: [
+          {
+            weight: "500 900",
+            style: "normal",
+            src: ["./src/assets/fonts/MerriweatherSans-VariableFont_wght.woff2"],
+            fallback: ["sans-serif"],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Merriweather",
+      cssVariable: "--typeface-body",
+      options: {
+        variants: [
+          {
+            weight: "200 600",
+            style: "normal",
+            src: ["./src/assets/fonts/Merriweather-VariableFont_opsz,wdth,wght.woff2"],
+            fallback: ["serif"],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Inter",
+      cssVariable: "--typeface-workhorse",
+      options: {
+        variants: [
+          {
+            weight: "200 600",
+            style: "normal",
+            src: ["./src/assets/fonts/Inter-VariableFont_opsz,wght.woff2"],
+            fallback: ["sans-serif"],
+          },
+        ],
+      },
+    },
+  ],
   experimental: {
     svgOptimizer: svgoOptimizer(),
   },
